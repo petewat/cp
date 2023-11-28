95d623c1b9dbd9ab52a08c4e86c361b11db0189f
petewat <101613113+petewat@users.noreply.github.com>
2023-11-28 19:49:38 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<stack>
+#include<map>
+#include<vector>
+#include<string>
+#include<unordered_map>
+#include <queue>
+#include<cstring>
+#include<limits.h>
+#include<cmath>
+#include<set>
+#include<algorithm>
+#include <iomanip>
+#include<numeric>
+#include<bitset>
+using namespace std;
+#define ll long long
+#define f first
+#define endl "\n"
+#define s second
+#define pii pair<int,int>
+#define ppii pair<int,pii>
+#define vi vector<int>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define F(n) for(int i=0;i<n;i++)
+#define lb lower_bound
+#define ub upper_bound
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+#pragma GCC optimize ("03,unroll-lopps")
+#define int long long
+using namespace std;
+const int mod=1e9+7,mxn=2e5+5,inf=1e18,minf=-1e18,lg=30;
+
+
+int32_t main(){
+    fastio
+}
+
